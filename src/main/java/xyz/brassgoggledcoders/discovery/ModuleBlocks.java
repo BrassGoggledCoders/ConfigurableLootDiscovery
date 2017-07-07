931d844059b4da17f0ecc77bf9b85a4fb06fd6cd
warlordjones <warlordjones@gmail.com>
2017-07-07 15:39:49 +0100

    Add config to generate syntax examples

--- src/main/java/xyz/brassgoggledcoders/discovery/ModuleBlocks.java
+++ src/main/java/xyz/brassgoggledcoders/discovery/ModuleBlocks.java
@@ -13,9 +13,11 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.teamacronymcoders.base.modulesystem.Module;
 import com.teamacronymcoders.base.modulesystem.ModuleBase;
+import com.teamacronymcoders.base.registrysystem.config.ConfigEntry;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.config.Property.Type;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Module(Discovery.MODID)
@@ -35,6 +37,12 @@ public class ModuleBlocks extends ModuleBase {
 	public void preInit(FMLPreInitializationEvent event) {
 		File jsonConfig =
 				new File(this.getConfigRegistry().getConfigFolder().getPath() + File.separator + "blocks.json");
+		this.getConfigRegistry().addEntry(new ConfigEntry("general", "enableExamples", Type.BOOLEAN, "false",
+				"Delete or rename blocks.json after enabling"));
+		if(this.getConfigRegistry().getBoolean("enableExamples", false)) {
+			lootEntries.put("dirt", new LootEntry[] {new LootEntry(KEY_NOTHING, "stick", 100),
+					new LootEntry("diamond_pickaxe", "diamond", 100)});
+		}
 		try {
 			if(!jsonConfig.exists() && jsonConfig.createNewFile()) {
 				String json = gson.toJson(lootEntries, new TypeToken<Map<String, LootEntry[]>>() {}.getType());
@@ -51,7 +59,6 @@ public class ModuleBlocks extends ModuleBase {
 		for(LootEntry[] loot : lootEntries.values()) {
 			this.getLogger().devInfo(Arrays.toString(loot));
 		}
-
 	}
 
 	public static class LootEntry {
