8bf531d25aba846709368a1d3624de471f191e1b
warlordjones <warlordjones@gmail.com>
2017-10-17 17:58:30 +0100

    Port to 1.12.2

--- src/main/java/xyz/brassgoggledcoders/discovery/Discovery.java
+++ src/main/java/xyz/brassgoggledcoders/discovery/Discovery.java
@@ -1,25 +1,47 @@
 package xyz.brassgoggledcoders.discovery;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.teamacronymcoders.base.BaseModFoundation;
 
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.*;
 
-// TODO Require enchantment. Block meta.
-@Mod(modid = Discovery.MODID, name = Discovery.MODNAME, version = Discovery.MODVERSION,
-		dependencies = Discovery.DEPENDS)
+// TODO Require enchantment. Block meta. Quantity ranges! item removal
+@Mod(modid = Discovery.MODID, name = Discovery.MODNAME, version = Discovery.MODVERSION, dependencies = Discovery.DEPENDS)
 public class Discovery extends BaseModFoundation<Discovery> {
 
+	public static class LootEntry {
+		public final String toolRep;
+		public final String lootRep;
+		public final int chance;
+
+		public LootEntry(String toolRep, String lootRep, int chance) {
+			this.toolRep = toolRep;
+			this.lootRep = lootRep;
+			this.chance = chance;
+		}
+
+		@Override
+		public String toString() {
+			return "TOOL: " + toolRep + " LOOT: " + lootRep + " CHANCE: " + chance;
+		}
+	}
+
 	public static final String MODID = "discovery";
-	public static final String MODNAME = "Discovery";
+	public static final String MODNAME = "Configurable Loot Discovery";
 	public static final String MODVERSION = "@VERSION@";
 	public static final String DEPENDS = "required-after:base@[0.0.0,];";
 
 	@Instance(MODID)
 	public static Discovery instance;
+	public static final String KEY_ANYTHING = "anything";
+	public static final String KEY_NOTHING = "nothing";
+	public static Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().disableHtmlEscaping().create();
 
 	public Discovery() {
 		super(MODID, MODNAME, MODVERSION, null);
@@ -34,7 +56,36 @@ public class Discovery extends BaseModFoundation<Discovery> {
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		super.preInit(event);
-		MinecraftForge.EVENT_BUS.register(new DiscoveryEventHandler());
+	}
+
+	@Override
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		super.init(event);
+	}
+
+	@Override
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		super.postInit(event);
+	}
+
+	public static ItemStack getStackFromStringRepresentation(String rep) {
+		String[] args = rep.split(":");
+		Item item = Item.getByNameOrId(args[0]);
+
+		if(args.length == 1) {
+			return new ItemStack(item);
+		}
+		else if(args.length == 2) {
+			int quantity = Integer.valueOf(args[1]);
+			return new ItemStack(item, quantity);
+		}
+		else {
+			int quantity = Integer.valueOf(args[1]);
+			int meta = Integer.valueOf(args[2]);
+			return new ItemStack(item, quantity, meta);
+		}
 	}
 
 }
--- src/main/java/xyz/brassgoggledcoders/discovery/ModuleBlocks.java
+++ src/main/java/xyz/brassgoggledcoders/discovery/ModuleBlocks.java
@@ -1,97 +1,99 @@
 package xyz.brassgoggledcoders.discovery;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.teamacronymcoders.base.modulesystem.Module;
 import com.teamacronymcoders.base.modulesystem.ModuleBase;
 import com.teamacronymcoders.base.registrysystem.config.ConfigEntry;
 
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Property.Type;
+import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Module(Discovery.MODID)
+@EventBusSubscriber(modid = Discovery.MODID)
 public class ModuleBlocks extends ModuleBase {
 
-	public static Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().disableHtmlEscaping().create();
-	public static Map<String, LootEntry[]> lootEntries = new HashMap<String, LootEntry[]>();
-	public static final String KEY_ANYTHING = "anything";
-	public static final String KEY_NOTHING = "nothing";
+	public static Map<String, Discovery.LootEntry[]> lootEntries = new HashMap<String, Discovery.LootEntry[]>();
 
 	@Override
 	public String getName() {
-		return "Main";
+		return "Blocks";
 	}
 
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
-		File jsonConfig =
-				new File(this.getConfigRegistry().getConfigFolder().getPath() + File.separator + "blocks.json");
-		this.getConfigRegistry().addEntry(new ConfigEntry("general", "enableExamples", Type.BOOLEAN, "false",
+		File jsonConfig = new File(getConfigRegistry().getConfigFolder().getPath() + File.separator + "blocks.json");
+		getConfigRegistry().addEntry(new ConfigEntry("general", "enableBlockExamples", Type.BOOLEAN, "false",
 				"Delete or rename blocks.json after enabling"));
-		if(this.getConfigRegistry().getBoolean("enableExamples", false)) {
-			lootEntries.put("dirt", new LootEntry[] {new LootEntry(KEY_NOTHING, "stick", 100),
-					new LootEntry("diamond_pickaxe", "diamond", 100)});
+		if(getConfigRegistry().getBoolean("enableBlockExamples", false)) {
+			lootEntries.put("dirt",
+					new Discovery.LootEntry[] { new Discovery.LootEntry(Discovery.KEY_NOTHING, "stick", 100),
+							new Discovery.LootEntry("diamond_pickaxe", "diamond", 100) });
 		}
 		try {
 			if(!jsonConfig.exists() && jsonConfig.createNewFile()) {
-				String json = gson.toJson(lootEntries, new TypeToken<Map<String, LootEntry[]>>() {}.getType());
+				String json = Discovery.gson.toJson(lootEntries, new TypeToken<Map<String, Discovery.LootEntry[]>>() {
+				}.getType());
 				FileWriter writer = new FileWriter(jsonConfig);
 				writer.write(json);
 				writer.close();
 			}
-			lootEntries =
-					gson.fromJson(new FileReader(jsonConfig), new TypeToken<Map<String, LootEntry[]>>() {}.getType());
+			lootEntries = Discovery.gson.fromJson(new FileReader(jsonConfig),
+					new TypeToken<Map<String, Discovery.LootEntry[]>>() {
+					}.getType());
 		}
 		catch(IOException e) {
-			this.getLogger().error("Error creating default configuration.");
+			getLogger().error("Error creating default configuration.");
 		}
-		for(LootEntry[] loot : lootEntries.values()) {
-			this.getLogger().devInfo(Arrays.toString(loot));
-		}
-	}
-
-	public static class LootEntry {
-		public final String toolRep;
-		public final String lootRep;
-		public final int chance;
-
-		public LootEntry(String toolRep, String lootRep, int chance) {
-			this.toolRep = toolRep;
-			this.lootRep = lootRep;
-			this.chance = chance;
-		}
-
-		@Override
-		public String toString() {
-			return "TOOL: " + toolRep + " LOOT: " + lootRep + " CHANCE: " + chance;
+		for(Discovery.LootEntry[] loot : lootEntries.values()) {
+			getLogger().devInfo(Arrays.toString(loot));
 		}
 	}
 
-	public static ItemStack getStackFromStringRepresentation(String rep) {
-		String[] args = rep.split(":");
-		Item item = Item.getByNameOrId(args[0]);
-
-		if(args.length == 1)
-			return new ItemStack(item);
-		else if(args.length == 2) {
-			int quantity = Integer.valueOf(args[1]);
-			return new ItemStack(item, quantity);
+	@SubscribeEvent
+	public static void onBlockHarvest(HarvestDropsEvent event) {
+		if(event.getWorld().isRemote || event.getHarvester() == null) {
+			return;
 		}
-		else {
-			int quantity = Integer.valueOf(args[1]);
-			int meta = Integer.valueOf(args[2]);
-			return new ItemStack(item, quantity, meta);
+		String blockName = event.getState().getBlock().getRegistryName().getResourcePath();
+		ItemStack held = event.getHarvester().getHeldItemMainhand();
+		if(ModuleBlocks.lootEntries.containsKey(blockName)) {
+			for(Discovery.LootEntry entry : ModuleBlocks.lootEntries.get(blockName)) {
+				if(entry.toolRep.equals(Discovery.KEY_ANYTHING)) {
+					if(event.getWorld().rand.nextInt(entry.chance) == 0) {
+						event.getDrops().add(Discovery.getStackFromStringRepresentation(entry.lootRep));
+					}
+				}
+				else if(entry.toolRep.equals(Discovery.KEY_NOTHING) && held == ItemStack.EMPTY) {
+					if(event.getWorld().rand.nextInt(entry.chance) == 0) {
+						event.getDrops().add(Discovery.getStackFromStringRepresentation(entry.lootRep));
+					}
+				}
+				else {
+					String itemName = held.getItem().getRegistryName().getResourcePath();
+					String[] item = entry.toolRep.split(":");
+					// Discovery.instance.getLogger().devInfo(Arrays.toString(item));
+					if(item[0].equals(itemName)) {
+						if(item.length == 1 || item[1].equals("*")) {
+							if(event.getWorld().rand.nextInt(entry.chance) == 0) {
+								event.getDrops().add(Discovery.getStackFromStringRepresentation(entry.lootRep));
+							}
+						}
+						else if(Integer.valueOf(item[1]) == held.getMetadata()) {
+							if(event.getWorld().rand.nextInt(entry.chance) == 0) {
+								event.getDrops().add(Discovery.getStackFromStringRepresentation(entry.lootRep));
+							}
+						}
+					}
+				}
+
+			}
 		}
 	}
 
